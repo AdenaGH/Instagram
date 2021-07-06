c3672dcae53bfc491b945a072a028bc1f38a04b9
AdenaGH <86276796+AdenaGH@users.noreply.github.com>
2021-07-06 16:30:48 -0400

    App Icon added

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+            configuration.applicationId = @"lmH7fH8oxkrcc1mvEWeh2PEhnIcVQsr5CagiyIrd";
+            configuration.clientKey = @"RsxQUDQXKa1Qp4OMVFQGbPGqFWYF1nC5EOq4w4MI";
+            configuration.server = @"https://parseapi.back4app.com";
+        }];
+
+        [Parse initializeWithConfiguration:config];
+    //Test
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+     gameScore[@"score"] = @1337;
+     gameScore[@"playerName"] = @"Sean Plott";
+     gameScore[@"cheatMode"] = @NO;
+     [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+      if (succeeded) {
+             NSLog(@"Object saved!");
+      } else {
+             NSLog(@"Error: %@", error.description);
+      }
+     }];
     return YES;
 }
 
